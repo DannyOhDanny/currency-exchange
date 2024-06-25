330463ea67b1ad9582f678299501f658ae7bc826
Daria Matveeva <119957276+DannyOhDanny@users.noreply.github.com>
2024-06-25 18:44:22 +0300

    Feat: markdown, styled, component logic

--- README.md
+++ README.md
@@ -4,6 +4,18 @@
 
 ## Ссылка на проект: https://dannyohdanny.github.io/currency-exchange/
 
+## Технологии
+
+<div >
+	<table>
+		<tr>
+            <td><code><img width="40" src="https://github.com/marwin1991/profile-technology-icons/assets/136815194/e56b5093-2f58-40cc-b194-5bdde41077b5" alt="Svelte" title="Svelte"/></code></td>
+			<td><code><img width="40" src="https://user-images.githubusercontent.com/25181517/192158954-f88b5814-d510-4564-b285-dff7d6400dad.png" alt="HTML" title="HTML"/></code></td>
+			<td><code><img width="40" src="https://user-images.githubusercontent.com/25181517/183898674-75a4a1b1-f960-4ea9-abcb-637170a00a75.png" alt="CSS" title="CSS"/></code></td>
+		</tr>
+	</table>
+</div>
+
 ## Пакеты API
 
 - API - @everapi/freecurrencyapi-js
--- src/App.svelte
+++ src/App.svelte
@@ -11,8 +11,6 @@ import {currencies}from './lib/currencies.svelte'
   
   const freecurrencyapi = new Freecurrencyapi('fca_live_9lFFlAQDT9tj88KOOYp5FiQUo03CpoUeqkQTcXVT');
 
-
-
   async function fetchExchangeRate() {
     try {
       const response = await freecurrencyapi.latest({
@@ -44,28 +42,26 @@ import {currencies}from './lib/currencies.svelte'
     await fetchExchangeRate();
   });
 
-  
-
 </script>
 
 
 <main class="converter">
   <div class="converter__title-wrapper">
-  <h1 class="converter__title">КОНВЕРТЕР ВАЛЮТ </h1>
-  <p class="converter__paragraph">{new Date().toLocaleDateString()}</p>
-</div>
-<div class="converter__input-wrapper">
+    <h1 class="converter__title">КОНВЕРТЕР ВАЛЮТ </h1>
+    <p class="converter__paragraph">{new Date().toLocaleDateString()}</p>
+  </div>
 
-<label for='sumInput' class="converter__label">Сумма для конвертации
-  <input type="number" bind:value={amountFrom} placeholder="Сумма для конвертации" on:input={updateExchangeRate} id='sumInput' class="converter__input">
-</label>
+  <div class="converter__input-wrapper">
+    <label for='sumInput' class="converter__label">Сумма для конвертации
+    <input type="number" bind:value={amountFrom} placeholder="Сумма для конвертации" on:input={updateExchangeRate} id='sumInput' class="converter__input">
+    </label>
 
 
-  <label for='currencyFrom' class="converter__label">
+    <label for='currencyFrom' class="converter__label">
     У меня есть
-    <select bind:value={currencyFrom} id='currencyFrom' on:change={updateExchangeRate} class="converter__select">
+      <select bind:value={currencyFrom} id='currencyFrom' on:change={updateExchangeRate} class="converter__select">
       {#each currencies as currency}
-        <option class="converter__option"value={currency.code}>{currency.name} - {currency.code} - {currency.flag}</option>
+         <option class="converter__option"value={currency.code}>{currency.name} - {currency.code} - {currency.flag}</option>
       {/each}
     </select>
   </label>
@@ -78,26 +74,38 @@ import {currencies}from './lib/currencies.svelte'
     </select>
   </label>
 
-
   {#if exchangeRate !== null && exchangeRate !== undefined && currencyFrom }
     <p class="converter__rate">1 {currencyFrom} = {exchangeRate} {currencyTo}</p>
-    {:else}
+  {:else}
     <p class="converter__rate"></p>
   {/if}
 
-  <p id='amount' class="converter__result">
-    ИТОГО в {currencyTo} : 
-   
-    {#if (amountFrom === 0 || !amountFrom ) }
-      <p class="converter__message">Введите сумму, которую вы хотите сконвертировать.</p>
 
-      {:else if (isNaN(amountTo) || !amountTo) }
-      <p class="converter__message">Введите валюту, в которую вы хотите сконвертировать вашу валюту.</p>
-      {:else }
-      <span class="converter__amount">{amountTo} {currencyTo}</span>
-      <p class="converter__message"></p>
+{#if amountFrom !== null && amountFrom !== 0 && currencyFrom}
+  <p id='amount' class="converter__result">
+    ИТОГО {amountFrom} {currencyFrom} : 
+
+    {#if isNaN(amountTo) || !amountTo}
+        <p class="converter__message">Введите валюту, в которую вы хотите сконвертировать.</p>
+    {:else}
+        <span class="converter__amount">{amountTo} {currencyTo}</span>
+        <p class="converter__message"></p>
+
     {/if}
   </p>
+{:else}
+  <p id='amount' class="converter__result">
+    ИТОГО : 
+  {#if amountFrom === 0 || isNaN(amountFrom) || amountFrom === null }
+     <p class="converter__message">Введите сумму, которую вы хотите сконвертировать.</p>
+  {:else if !currencyFrom}
+     <p class="converter__message">Введите валюту, из которой вы хотите сконвертировать.</p>
+    {:else}
+    <p class="converter__message"></p>
+  {/if}
+
+{/if}
+
 </div>
 </main>
 
@@ -108,13 +116,13 @@ import {currencies}from './lib/currencies.svelte'
     margin: 0 auto;
     padding: 40px 50px 30px 50px ;
     border-radius: 12px;
-    background-color: white;
+    background-color: var(--white-color);
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
     gap : 10px;
     width: 300px;
-    box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
+    box-shadow: var(--background-shadow);
   }
   .converter__title-wrapper {
     display: flex;
@@ -127,7 +135,7 @@ import {currencies}from './lib/currencies.svelte'
     text-align: left;
     font-weight: 900;
     font-size: 18px;
-    color:#062263;
+    color:var(--main-color);
    }
    .converter__paragraph {
     display: flex;
@@ -136,8 +144,8 @@ import {currencies}from './lib/currencies.svelte'
     padding: 5px;
     font-size: 14px;
     font-weight: 700;
-    color: white;
-    background-color: #8C6A82;
+    color:var(--white-color);
+    background-color: var(--secondary-color);
     border-radius: 8px;
     text-align: center;
    }  
@@ -153,63 +161,64 @@ import {currencies}from './lib/currencies.svelte'
     flex-direction: column;
     align-self: flex-start;      
     font-size: 14px;
-    color: gray;
+    color: var(--gray-color);
     max-width: 228px; 
     width: 100%;
     font-weight: 200;
+
     }
   .converter__input {
     box-sizing: border-box;    
     border-radius: 5px;
-    color: #272727;
-    border: 1px solid  #446EAE;
-    background-color: white;
-    box-shadow: 5px 5px 5px #446EAE;
+    color: var(--black-color);
+    border: 1px solid  var(--shadow-color);
+    background-color: var(--white-color);
+    box-shadow: 5px 5px 5px var(--shadow-color);
     height: 30px;
     font-size: 10px;
+    padding-left: 10px;
+    margin-top: 2px;
   }
   
   input::placeholder{
     font-size: 14px;
   }
   .converter__input:focus, .converter__input:active, .converter__input:visited{
-    border: 1px solid rgb(33, 178, 178)
+    border: 1px solid var(--secondary-color);
   }
   .converter__select {
     border-radius: 5px;
     color: #272727;
-    border: 1px solid  #446EAE;
-    background-color: white;
-    box-shadow: 5px 5px 5px #446EAE;
+    border: 1px solid  var(--shadow-color);
+    background-color: var(--white-color);
+    box-shadow: 5px 5px 5px var(--shadow-color);
     height: 30px;
-    font-size: 12px; 
+    font-size: 12px;
+    padding: 5px; 
+    margin-top: 2px;
   }
   .converter__select:focus, .converter__select:active, .converter__select:visited{
-    border: 1px solid rgb(33, 178, 178);
+    border: 1px solid var(--secondary-color);
   }  
   
   .converter__rate {
     height:22.5px;
     font-size: 16px;
     font-weight: 200;
-    color: #062263;
-
+    color: var(--main-color);
   }
-
   .converter__result {
-    color: #062263;
+    color: var(--main-color);
     font-size: 18px; 
-    font-weight: 600;
+    font-weight: 900;
   }
-  
   .converter__rate, .converter__result {
     text-align: start; 
     margin:0;
     padding:0;
   }
-   
   .converter__message {
-      color: red;
+      color: var(--error-color);
       font-size: 10px;
       font-weight: 300;
       height: 22.5px;
@@ -217,7 +226,7 @@ import {currencies}from './lib/currencies.svelte'
     }
 
   .converter__amount {
-       color: rgb(33, 178, 178);; 
+       color: var(--secondary-color); 
        font-size: 18px;
        font-weight: 800;}
 
